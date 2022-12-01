5deab8bef78a6cae8521799620c7e654692e7107
Jakub Malczewski <54622856+jakubmalczewski@users.noreply.github.com>
2022-12-01 20:44:42 +0100

    day1-better

--- day1/day1.py
+++ day1/day1.py
@@ -2,17 +2,14 @@ with open("day1/input.txt") as file:
     food = file.read()
 
 mostcalo = max(
-    sum(int(meal) for meal in elf.split("\n") if meal != "")
-    for elf in food.split("\n\n")
+    calolist := [sum(int(meal) for meal in elf.split("\n") if meal != "")
+    for elf in food.split("\n\n")]
 )
 print(mostcalo)
 
 top3 = sum(
     sorted(
-        [
-            sum(int(meal) for meal in elf.split("\n") if meal != "")
-            for elf in food.split("\n\n")
-        ],
+        calolist,
         reverse=True,
     )[:3]
 )
