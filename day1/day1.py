1fa975a34ce1f9fb0fdf598a651094dc4af45ef7
Jakub Malczewski <54622856+jakubmalczewski@users.noreply.github.com>
2022-12-01 20:33:49 +0100

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,19 @@
-    
+with open("day1/input.txt") as file:
+    food = file.read()
+
+mostcalo = max(
+    sum(int(meal) for meal in elf.split("\n") if meal != "")
+    for elf in food.split("\n\n")
+)
+print(mostcalo)
+
+top3 = sum(
+    sorted(
+        [
+            sum(int(meal) for meal in elf.split("\n") if meal != "")
+            for elf in food.split("\n\n")
+        ],
+        reverse=True,
+    )[:3]
+)
+print(top3)
